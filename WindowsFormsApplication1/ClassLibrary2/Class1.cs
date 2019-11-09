d785a18c70208aa0ddaff202486159889a4995cd
OI-TEAM <alex.hernandez016@gmial.com>
2019-11-09 12:27:28 -0600

    Se crearon las librerias nesecarias para el funcionamiento del sistema prototipo

---
+++ WindowsFormsApplication1/ClassLibrary2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace cconectar
+{
+    public class utilidades
+    {
+        public static DataSet ejecutar(string instruccion)
+        {
+            SqlConnection abrir = new SqlConnection(@"Data Source=DESKTOP-S9OPL2R\SQLEXPRESS;Initial Catalog=deposito;Integrated Security=True");
+            abrir.Open();
+            DataSet carrito = new DataSet();
+            SqlDataAdapter adaptador = new SqlDataAdapter(instruccion,abrir);
+            adaptador.Fill(carrito);
+            abrir.Close();
+
+            return (carrito);
+        }
+         
+    } }
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/inicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using cconectar;
+
+namespace WindowsFormsApplication1
+{
+    public partial class inicio : Form
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+       
